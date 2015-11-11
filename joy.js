3d26f39e44d607912453faf732302b627c9c84e8
Regis d'Aubarede <regis.aubarede@gmail.com>
2015-11-11 21:29:49 +0100

    update

--- joy.js
+++ joy.js
@@ -12,6 +12,11 @@ function initWebSocket(p) {
       });
     });
     wss.broadcast = function broadcast(data) {
+      if (data.length==0) {
+          message("ws","close all connections");
+          wss.clients.forEach(function each(client) { client.close() });
+          return;
+      }
       message("ws","nbClients="+wss.clients.length);
       wss.clients.forEach(function each(client) {
         client.send(data,function ack(error) {message("ws","error sending : "+error.toString());});
@@ -39,17 +44,36 @@ function position(id,x,y) {
 }
 var oldText="";
 var nbm=0;
-function transmit(text) {
+function transmit(data) {
+  text=data.toString();
   if (oldText===text) return;
   if (broadcast) {
     oldText=text;
-    text=text.replace("A",""+nbm+","+(( new Date().getTime())%1000));
+    data[4]=nbm
+    data[5]=( new Date().getTime() ) % 1000;
     nbm=(nbm+1)%1000;
+    text=JSON.stringify(data);
     message("ws2","sending "+text + " at " + (( new Date().getTime())%1000));
     broadcast(text);
   }
 }
-
+var mouse=false;
+function mousepush() { mouse=true; }
+function mouserelease(event) { 
+  mouse=false;
+  position("cross",event.offsetX-5,event.offsetY-5);  
+}
+function mousemove(event) {
+   if (! mouse) return(flse);
+   event.stopPropagation()
+   position("cross",-10000,-10000);
+   //message("a1","mouse : " + event.offsetX + "/" + event.offsetY);
+   transmit([event.offsetX,event.offsetY,0,[0,0,0,0,0,0],-100,0]);
+   return(true);
+}
+//=============================================== 
+// Gamepad Agent
+//=============================================== 
 function mloop() {
   try {
     loop(); 
@@ -69,12 +93,15 @@ function loop() {
  }
  if (! gpok) return;
  
- gp=((navigator.webkitGetGamepads && navigator.webkitGetGamepads()) || navigator.getGamepads())[0];// il faut relire la liste de gamepad: elle est dynamique (sous chrome)...
+ // il faut relire la liste de gamepad: elle est dynamique (sous chrome)...
+ gp=((navigator.webkitGetGamepads && navigator.webkitGetGamepads()) || navigator.getGamepads())[0];
+ 
  var str1="buttons states=";
  for (var ib=0;ib<6;ib++) str1=str1+" "+ (gp.buttons[ib].pressed ? ("<b>B"+(ib+1)+"</b>") : "B"+(ib+1))
+ message("b1",str1);
+
  var vb=[0,0,0,0,0,0]
  for (var ib=0;ib<6;ib++) vb[ib]=gp.buttons[ib].pressed ? 1 : 0 ;
- message("b1",str1);
 
  //message("a1",""+Math.round(gp.axes[0]*1000));
  //message("a2",""+Math.round(gp.axes[1]*1000));
@@ -83,8 +110,8 @@ function loop() {
  var x=1+gp.axes[0];
  var y=1+gp.axes[1];
  var z=1+gp.axes[2];
- position("croix",x*100-5,y*100-5);
- transmit("["+Math.round(x*100)+","+Math.round(y*100)+","+Math.round(z*100)+",["+vb.join(",")+"],A]");
+ position("cross",x*100-5,y*100-5);
+ transmit([Math.round(x*100),Math.round(y*100),Math.round(z*100),vb,-100,0]);
 }
 
 var idi=null;
